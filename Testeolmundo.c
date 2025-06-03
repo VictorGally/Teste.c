c67a14907766c840a911c7cdbb704ac31f7ff9d7
Victor Gally <victor.gally@gmail.com>
2025-06-03 13:41:18 +0000

    Sala de aula

--- Testeolmundo.c
+++ Testeolmundo.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+int main(){
+    char letra;
+    
+    printf("Entre com uma letras minuscula:\n");
+    scanf("%c", &letra);
+    letra=letra+( 'A' - 'a' );
+    printf("Caractere=%c \n", letra);
 
+    return o;
+
+}
